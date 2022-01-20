1b2e1ecef8ddd161bdbf0d6a4a90b392ebe656cd
Thom <81919273+leeyou34@users.noreply.github.com>
2022-01-21 00:07:40 +0900

    Create 1000.java

---
+++ 1000.java
@@ -0,0 +1,22 @@
+/*
+* 문제 (1000)
+* 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+* 입력
+* 첫째 줄에 A와 B가 주어진다. (0 < A, B < 10)
+* 출력
+* 첫째 줄에 A+B를 출력한다.
+*
+* Below is the answer
+*/
+
+import java.util.Scanner;
+public class Main{
+    public static void main(String[]args) {
+        Scanner sc = new Scanner(System.in);
+        int A, B;
+        A=sc.nextInt();
+        B=sc.nextInt();
+        System.out.println(A+B);
+    }
+    
+}
