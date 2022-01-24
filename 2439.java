5a0aabb56bad293bd6ed16336228b7982f679b18
Thom <81919273+leeyou34@users.noreply.github.com>
2022-01-24 22:57:25 +0900

    Create 2439.java

---
+++ 2439.java
@@ -0,0 +1,38 @@
+/*
+문제
+첫째 줄에는 별 1개, 둘째 줄에는 별 2개, N번째 줄에는 별 N개를 찍는 문제
+
+하지만, 오른쪽을 기준으로 정렬한 별(예제 참고)을 출력하시오.
+
+입력
+첫째 줄에 N(1 ≤ N ≤ 100)이 주어진다.
+
+출력
+첫째 줄부터 N번째 줄까지 차례대로 별을 출력한다.
+
+answer is listed below...
+*/
+
+import java.util.Scanner;
+
+public class Main{
+      public static void main(String[]args){
+      Scanner sc = new Scanner(System.in);
+          int n = sc.nextInt();
+          
+          if(1<= n && n <=100){
+             for(int i=1; i<=n; i++){
+                for(int j=n; j>0; j--){
+                    if(j>i){
+                     System.out.print(" ");
+                    } else {
+                     System.out.print("*");
+                    }
+                }
+             System.out.println();
+             }
+          } else{
+             System.out.println("please put the value between 1~100");
+          }
+      }
+}
