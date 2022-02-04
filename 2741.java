304e3879f14f0f74c72b3b8138bb1f42798f3ded
Thom <81919273+leeyou34@users.noreply.github.com>
2022-02-04 11:18:07 +0900

    Create 2741.java

---
+++ 2741.java
@@ -0,0 +1,31 @@
+/*
+
+문제
+자연수 N이 주어졌을 때, 1부터 N까지 한 줄에 하나씩 출력하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 100,000보다 작거나 같은 자연수 N이 주어진다.
+
+출력
+첫째 줄부터 N번째 줄 까지 차례대로 출력한다.
+
+
+
+*/
+
+import java.util.Scanner;
+
+public class Main{
+	public static void main(String[]args) {
+		Scanner sc= new Scanner(System.in);
+		int n= sc.nextInt();
+
+		if(1<=n && n<=100000) {
+			for(int i=1; i<=n; i++) {
+				System.out.println(i);
+			}
+		} else {
+			System.out.println("pleaes put the numerical value between 1~10,000");
+		}
+	}
+}
