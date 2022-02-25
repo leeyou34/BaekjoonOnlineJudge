f880ebbebeb7aedaa9f3738aa0a9c16c68344d82
Thom <81919273+leeyou34@users.noreply.github.com>
2022-02-25 18:01:16 +0900

    Create 10172.java

---
+++ 10172.java
@@ -0,0 +1,23 @@
+/*
+
+문제
+아래 예제와 같이 개를 출력하시오.
+
+입력
+없음.
+
+출력
+개를 출력한다.
+
+
+*/
+
+public class Main{
+    public static void main(String[]args){
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");
+    }
+}
