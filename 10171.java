1d4175e04101868e4c2648aff6f90062c3569350
Thom <81919273+leeyou34@users.noreply.github.com>
2022-02-25 18:00:15 +0900

    Create 10171.java

---
+++ 10171.java
@@ -0,0 +1,23 @@
+/*
+
+문제
+아래 예제와 같이 고양이를 출력하시오.
+
+입력
+없음.
+
+출력
+고양이를 출력한다.
+
+
+*/
+
+public class Main {
+    public static void main(String[]args){
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+    
+}
