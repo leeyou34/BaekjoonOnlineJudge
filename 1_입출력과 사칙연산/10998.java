360bf9d47d56d8c07b44a9af31fca2e162807ffe
Thom <81919273+leeyou34@users.noreply.github.com>
2023-05-04 21:29:26 +0900

    Create 10998.java

---
+++ 1_입출력과 사칙연산/10998.java
@@ -0,0 +1,31 @@
+/*
+문제
+두 정수 A와 B를 입력받은 다음, A×B를 출력하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 A와 B가 주어진다. (0 < A, B < 10)
+
+출력
+첫째 줄에 A×B를 출력한다.
+*/
+
+
+import java.util.Scanner;
+
+public class Main{
+	public static void main(String [] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		
+		int result = A*B;
+		
+		if(0< A && A <10 ){
+			if(0< B && B<10) {
+				System.out.println(result);
+			} 
+		}	
+	}
+}
